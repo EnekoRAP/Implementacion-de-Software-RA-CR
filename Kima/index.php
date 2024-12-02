c535b45a07ee965eae5b201d91e35e0ca593bd6b
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-12-02 15:24:40 -0600

    Crashed project

--- Kima/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-$title = "Inicio"; // Título dinámico
-include 'layout.php'; // Incluir la plantilla base>
--- Kima/layouts/footer.php
+++
@@ -1,6 +0,0 @@
-<?php
-$fecha = date("d/m/Y")
-?>
-<footer>
-    <p>&copy; <?php echo $fecha?> Kima. Todos los derechos reservados.</p>
-</footer>
