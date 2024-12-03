6471b5728d8c85f91afc7147d782932c205f45ad
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-12-03 11:18:41 -0600

    Update layout.php

--- Kima/layout.php
+++ Kima/layout.php
@@ -1,31 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($title) ? $title : "Mi Proyecto"; ?></title>
     <!-- Cargar CSS -->
-    <link href="assets/css/style.bundle.css" rel="stylesheet">
+    <link href="/Kima/assets/css/style.bundle.css" rel="stylesheet">
 </head>
-<body id="kt_app_body" class="app-default">
-    <div id="kt_app_root">
-        <!-- Sidebar -->
-        <?php include 'layouts/sidebar.php'; ?>
 
-        <!-- Contenido Principal -->
-        <div id="kt_app_content">
-            <?php
-            // Cargar contenido dinámico
-            if (isset($content)) {
-                include $content;
-            } else {
-                
-            }
-            ?>
+<body id="kt_app_body" class="app-default">
+    <div id="kt_app_root" class="d-flex flex-column flex-root">
+        <div class="d-flex flex-row flex-column-fluid">
+            <!-- Sidebar -->
+            <?php include 'layouts/sidebar.php'; ?>
+
+            <!-- Contenido Principal -->
+            <div id="kt_app_content" class="flex-column flex-row-fluid" style="margin-left: 250px; padding: 20px;">
+                <?php
+                if (isset($content)) {
+                    include $content;
+                } else {
+                    echo "<div>No se encontró contenido.</div>";
+                }
+                ?>
+            </div>
         </div>
     </div>
-
-    <!-- Cargar JS -->
-    <script src="assets/js/scripts.bundle.js"></script>
 </body>
+
 </html>
